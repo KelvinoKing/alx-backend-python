1194d80482bf6145f494b53e0c98035dd7d28453
KelvinoKing <kelvinoking@outlook.com>
2024-04-08 11:19:03 +0300

    Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -15,9 +15,13 @@ For each of these inputs, test with assertEqual that the function
 returns the expected result.
 """
 import unittest
-from utils import access_nested_map
 from parameterized import parameterized
-from typing import Dict, Tuple, Any, Union
+from typing import Dict, Tuple, Union
+from utils import (
+    access_nested_map,
+    get_json,
+    memoize,
+)
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -28,9 +32,11 @@ class TestAccessNestedMap(unittest.TestCase):
             ({"a": {"b": 2}}, ("a",), {"b": 2}),
             ({"a": {"b": 2}}, ("a", "b"), 2)
         ])
-    def test_access_nested_map(self,
-                               nested_map: Dict,
-                               path: Tuple[str],
-                               expected: Union[Dict, int]) -> None:
+    def test_access_nested_map(
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            expected: Union[Dict, int],
+            ) -> None:
         """test_access_nested_map method"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
